0921b4412c1607c6b2bc172ce521d05cb50ccbe1
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-02-03 23:04:17 +0530

    Fetch initial data with useEffect in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './counter'
+import QuoteFetcher from './QuoteFetcher'
 
 function App() {
 
 
   return (
     <>
-      <Counter />
+      <QuoteFetcher />
+      {/* <Counter /> */}
     </>
   )
 }
